3402523b88c52681954350bca315a754ea780ba1
frenchkebab <tubowangin@naver.com>
2019-01-25 00:04:54 +0900

    form 3개 어떻게 할지 몰라서 걍 주석처리

--- hiarc_registration/admin.py
+++ hiarc_registration/admin.py
@@ -5,20 +5,19 @@ from django.contrib.auth.admin import UserAdmin
 from import_export import resources
 from import_export.admin import ImportExportModelAdmin
 
-from .forms import HiarcUserCreationForm 
+from .forms import HiarcUserCreationForm1, HiarcUserCreationForm2, HiarcUserCreationForm3
 from .models import HiarcUser
 
 class HiarcUserResource(resources.ModelResource):
     class Meta:
         model = HiarcUser
-        fields = ('email', 'username', 'real_name', 'status', 
-                    'phone_number', 'motivation', 'portfolio', 'is_staff', 'is_active', )
+        fields = ('email', 'username', 'real_name', 'first_name' 'status', 'last_name', 'phone_number', 'motivation', 'portfolio', 'is_staff', 'is_active', )
 
 # Register your models here.
 class HiarcUserAdmin(ImportExportModelAdmin, UserAdmin):
     resource_class = HiarcUserResource
-    form = HiarcUserCreationForm 
-    list_display = ("email", "real_name", "status", "phone_number", "motivation", "portfolio", "is_staff", "is_active")
+    # form = [HiarcUserCreationForm1, HiarcUserCreationForm2, HiarcUserCreationForm3]
+    list_display = ("email", "real_name", "first_name", "last_name", "status", "phone_number", "motivation", "portfolio", "is_staff", "is_active")
     list_filter = ["is_staff", "is_active"]
 
     fieldsets =  (
